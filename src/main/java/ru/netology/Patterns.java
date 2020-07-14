d16372b6a907effdf2527161a519cd876ae1223a
AlikAskat <a-r-b@bk.ru>
2020-07-14 17:50:17 +0500

    Update Patterns.java

--- src/main/java/ru/netology/Patterns.java
+++ src/main/java/ru/netology/Patterns.java
@@ -1,4 +1,27 @@
 package ru.netology;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 public class Patterns {
+
+
+    public static class AppDataGeneration {
+        private AppDataGeneration() {
+        }
+
+        @Data
+        @RequiredArgsConstructor
+
+        public static class AutoData {
+            private final String cityName;
+            private final String date;
+            private final String userName;
+            private final String phone;
+        }
+
+        public static String getUserName() {
+            return "Вася Вася";
+        }
+    }
 }
