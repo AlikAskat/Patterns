686cb1dcdad7c468534acaabbb5019d80dce6691
AlikAskat <a-r-b@bk.ru>
2020-07-14 20:01:02 +0500

    Create DataGenerator.java

---
+++ src/main/java/ru/netology/DataGenerator.java
@@ -0,0 +1,32 @@
+package ru.netology;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+public class DataGenerator {
+
+
+
+    public static String getUserName(String s) {
+        return null;
+    }
+
+    public static class AppDataGeneration {
+        private AppDataGeneration() {
+        }
+
+        @Data
+        @RequiredArgsConstructor
+
+        public static class AutoData {
+            private final String cityName;
+            private final String date;
+            private final String userName;
+            private final String phone;
+        }
+
+        public static String getUserName() {
+            return "Вася Вася";
+        }
+    }
+}
