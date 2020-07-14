1123fdc50a3e302a10d162e036ac67c2c2f7500c
AlikAskat <a-r-b@bk.ru>
2020-07-14 17:50:07 +0500

    Update PatternsTest.java

--- src/test/java/ru/netology/PatternsTest.java
+++ src/test/java/ru/netology/PatternsTest.java
@@ -2,15 +2,21 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class PatternsTest {
 
-//    private Object button;
+    //private Object button;
 
     @Test
     public void shouldChangeOfDeliveryDate() {
@@ -23,6 +29,35 @@ class PatternsTest {
         form.$("[type='button']").click();
         $(byText("Забронировать")).click();
         $("[data-test-id='notification']").waitUntil(Condition.visible, 15000);
+    }
+    static class AppDataGenerationTest {
+
+        //private Object String;
+
+        @BeforeEach
+        void getUp() {
+            new Faker(new Locale("ru-RU"));
+
+        }
+
+
+        @Test
+        public void whenLetterifyCalled__checkPatternMatches() {
+            FakeValuesService fakeValuesService = new FakeValuesService(new Locale("ru-RU")
+                    ,new RandomService());
+            String name = fakeValuesService.letterify("??Вася ??Вася");
+            Matcher nameMatcher = Pattern.compile( "П??етя ??Петя").matcher(name);
+
+        }
+
+
+        @Test
+        void getUserName(String faker) {
+            String name = String faker(name);
+            new Faker(new Locale("ru-RU"));
+            System.out.println(getUserName("Вася Вася"));
+        }
+
     }
 
 
