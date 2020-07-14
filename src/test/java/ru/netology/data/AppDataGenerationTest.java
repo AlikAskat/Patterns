4948b986f98304f55e104f8d2fc082d9d80e2923
AlikAskat <a-r-b@bk.ru>
2020-07-14 12:12:23 +0500

    Create AppDataGenerationTest.java

---
+++ src/test/java/ru/netology/data/AppDataGenerationTest.java
@@ -0,0 +1,35 @@
+package ru.netology.data;
+
+import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+
+
+class AppDataGenerationTest {
+
+    @BeforeEach
+    void getUp() {
+        new Faker(new Locale("ru-RU"));
+
+    }
+
+
+    @Test
+    public void whenLetterifyCalled__checkPatternMatches() {
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("ru-RU")
+                , new RandomService());
+        String name = fakeValuesService.letterify("??Вася ??Вася");
+        Matcher nameMatcher = Pattern.compile("П??етя ??Петя").matcher(name);
+
+    }
+
+
+}
