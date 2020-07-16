dd2d048497aa0fa6413abc88d21988a23ddfcfb3
AlikAskat <a-r-b@bk.ru>
2020-07-17 02:00:36 +0500

    Update DataGenerator.java

--- src/test/java/ru/netology/DataGenerator.java
+++ src/test/java/ru/netology/DataGenerator.java
@@ -1,6 +1,15 @@
 package ru.netology;
+
 import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+
 import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DataGenerator {
     private static Faker faker = new Faker(new Locale("ru"));
     public DataGenerator() {
@@ -9,8 +18,17 @@ public class DataGenerator {
         return faker.name().lastName() + " " + faker.name().firstName();
     }
     public static String getPhoneNumber() {
-        return faker.phoneNumber().phoneNumber();
+//        return faker.phoneNumber();
+        FakeValuesService fakeValuesService = new FakeValuesService(
+                new Locale("ru"), new RandomService());
+
+        String alphaNumericString = fakeValuesService.regexify("[+][7][9][019]{9}");
+        Matcher alphaNumericMatcher = Pattern.compile("[+][7][9][019]{9}").matcher(alphaNumericString);
+
+        assertTrue(alphaNumericMatcher.find());
+        return alphaNumericString;
     }
+
     public static String getCity() {
         String[] cities = new String[]{"Москва", "Воронеж", "Краснодар", "Владивосток"};
         int itemIndex = (int) (Math.random() * cities.length);
