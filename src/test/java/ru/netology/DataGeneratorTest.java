78d1c35a1c136461713fb5dc4ea2952198ab6730
AlikAskat <a-r-b@bk.ru>
2020-07-16 09:11:19 +0500

    Update DataGeneratorTest.java

--- src/test/java/ru/netology/DataGeneratorTest.java
+++ src/test/java/ru/netology/DataGeneratorTest.java
@@ -7,43 +7,42 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Locale;
-import java.util.prefs.AbstractPreferences;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class DataGeneratorTest {
+//
+//
+//
+//    @BeforeEach
+//    void setUpAll() {
+//        new Faker( new Locale( "ru-RU" ) );
+//    }
+//
+//    @Test
+//    void getUserName() {
+//        Faker faker = new Faker();
+//        String name = faker.name().fullName();
+//        $( "[name='name']" ).setValue( DataGenerator.getUserName( "['Вася Вася']" ) );
 
-
-    private AbstractPreferences faker;
-
-    @BeforeEach
-    void setUpAll() {
-        new Faker( new Locale( "ru-RU" ) );
-    }
 
     @Test
-    void getUserName() {
-        Faker faker = new Faker();
-        String name = faker.name().fullName();
+    public void shouldChangeOfDeliveryDate() {
+        open( "http://localhost:9999" );
+        SelenideElement form = $( "[class='App_appContainer__3jRx1']" );
+        form.$( "[placeholder='Город']" ).setValue( "Тамбов" );
+        //form.$("[name='name']").setValue("Вася Вася");
         $( "[name='name']" ).setValue( DataGenerator.getUserName( "['Вася Вася']" ) );
+        form.$( "[name='phone']" ).setValue( "+79120000000" );
+        form.$( "[class='checkbox__box']" ).click();
+        form.$( "[type='button']" ).click();
+        $( byText( "Забронировать" ) ).click();
+        $( "[data-test-id='notification']" ).waitUntil( Condition.visible, 15000 );
     }
-
-
-    @Test
-    public void shouldChangeOfDeliveryDate(){
-        open("http://localhost:9999");
-        SelenideElement form = $("[class='App_appContainer__3jRx1']");
-        form.$("[placeholder='Город']").setValue("Тамбов");
-        form.$("[name='name']").setValue("Вася Вася");
-        form.$("[name='phone']").setValue("+79120000000");
-        form.$("[class='checkbox__box']").click();
-        form.$("[type='button']").click();
-        $(byText("Забронировать")).click();
-        $("[data-test-id='notification']").waitUntil(Condition.visible, 15000);
-    }
-
-
-
 }
+
+
+
+
