66e89479079deecc448078ce59213c801589ea74
AlikAskat <a-r-b@bk.ru>
2020-07-16 14:40:50 +0500

    Update DataGeneratorTest.java

--- src/test/java/ru/netology/DataGeneratorTest.java
+++ src/test/java/ru/netology/DataGeneratorTest.java
@@ -10,7 +10,8 @@ import java.util.Locale;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static ru.netology.DataGenerator.*;
+import static ru.netology.DataGenerator.getUserName;
+import static ru.netology.DataGenerator.getUserName.*;
 
 class DataGeneratorTest {
 
@@ -20,16 +21,20 @@ class DataGeneratorTest {
     public void shouldChangeOfDeliveryDate() {
         new Faker( new Locale( "ru-RU" ) );
         open( "http://localhost:9999" );
-        SelenideElement form = $( "[class='App_appContainer__3jRx1']" );
-        form.$( "[placeholder='Город']" ).setValue( "Тамбов" );
+        $( "[class='App_appContainer__3jRx1']" );
+        $( "[placeholder='Город']" ).setValue( String.valueOf( new cityName( "" ) ) );
         $( "[name='name']" ).setValue( String.valueOf( new getUserName( "" ) ) );
-        form.$( "[name='phone']" ).setValue( "+79120000000" );
-        form.$( "[class='checkbox__box']" ).click();
-        form.$( "[type='button']" ).click();
+        $( "[name='phone']" ).setValue( String.valueOf( new phoneNumber( "" ) ) );
+        $( "[class='checkbox__box']" ).click();
+        $( "[type='button']" ).click();
         $( byText( "Забронировать" ) ).click();
-        $( "[data-test-id='notification']" ).waitUntil( Condition.visible, 15000 );
+        $( "[data-test-id='notification']" ).waitUntil( Condition.visible, 10000 );
     }
+
+
+
 }
+
 
 
 
