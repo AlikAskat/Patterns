f48071975450254a076600149b2606797372f667
AlikAskat <a-r-b@bk.ru>
2020-07-17 02:00:32 +0500

    Update DataGeneratorTest.java

--- src/test/java/ru/netology/DataGeneratorTest.java
+++ src/test/java/ru/netology/DataGeneratorTest.java
@@ -21,7 +21,7 @@ class DataGeneratorTest {
         $( "[class='App_appContainer__3jRx1']" );
         new DataGenerator();
         $( "[placeholder='Город']" ).setValue( String.valueOf( DataGenerator.getCity() ) );
-        $( "[name='name']" ).setValue( String.valueOf( new DataGenerator().getUserName() ) );
+        $( "[name='name']" ).setValue( new DataGenerator().getUserName() );
         $( "[name='phone']" ).setValue( String.valueOf( new DataGenerator().getPhoneNumber() ) );
         $( "[class='checkbox__box']" ).click();
         $( "[type='button']" ).click();
