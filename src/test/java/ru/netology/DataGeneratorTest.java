a2b14f8ecf4bef0252b1e17f7c1b6d5e6386079f
AlikAskat <a-r-b@bk.ru>
2020-07-16 09:30:21 +0500

    Update DataGeneratorTest.java

--- src/test/java/ru/netology/DataGeneratorTest.java
+++ src/test/java/ru/netology/DataGeneratorTest.java
@@ -2,30 +2,15 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-import com.github.javafaker.Faker;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Locale;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 class DataGeneratorTest {
-//
-//
-//
-//    @BeforeEach
-//    void setUpAll() {
-//        new Faker( new Locale( "ru-RU" ) );
-//    }
-//
-//    @Test
-//    void getUserName() {
-//        Faker faker = new Faker();
-//        String name = faker.name().fullName();
-//        $( "[name='name']" ).setValue( DataGenerator.getUserName( "['Вася Вася']" ) );
+
 
 
     @Test
@@ -33,8 +18,7 @@ class DataGeneratorTest {
         open( "http://localhost:9999" );
         SelenideElement form = $( "[class='App_appContainer__3jRx1']" );
         form.$( "[placeholder='Город']" ).setValue( "Тамбов" );
-        //form.$("[name='name']").setValue("Вася Вася");
-        $( "[name='name']" ).setValue( String.valueOf( new DataGenerator.getUserName( "['Вася Вася']" ) ) );
+        $( "[name='name']" ).setValue( String.valueOf( new DataGenerator.getUserName( "" ) ) );
         form.$( "[name='phone']" ).setValue( "+79120000000" );
         form.$( "[class='checkbox__box']" ).click();
         form.$( "[type='button']" ).click();
