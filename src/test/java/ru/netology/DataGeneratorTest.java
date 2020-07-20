5eaa41fa1cddb5eaeabfa86bd7a85ae8ae4d6fd3
AlikAskat <a-r-b@bk.ru>
2020-07-20 23:52:40 +0500

    Update DataGeneratorTest.java

--- src/test/java/ru/netology/DataGeneratorTest.java
+++ src/test/java/ru/netology/DataGeneratorTest.java
@@ -3,12 +3,15 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
+import ru.netology.util.DataGenerator;
 
 import java.util.Locale;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static java.lang.String.*;
+import static ru.netology.util.DataGenerator.*;
 
 class DataGeneratorTest {
 
@@ -16,13 +19,13 @@ class DataGeneratorTest {
 
     @Test
     public void shouldChangeOfDeliveryDate() {
-        new Faker( new Locale( "ru-RU" ) );
+        new Faker( new Locale( "ru" ) );
         open( "http://localhost:9999" );
         $( "[class='App_appContainer__3jRx1']" );
         new DataGenerator();
-        $( "[placeholder='Город']" ).setValue( String.valueOf( DataGenerator.getCity() ) );
+        $( "[placeholder='Город']" ).setValue( valueOf( getCity() ) );
         $( "[name='name']" ).setValue( new DataGenerator().getUserName() );
-        $( "[name='phone']" ).setValue( String.valueOf( new DataGenerator().getPhoneNumber() ) );
+        $( "[name='phone']" ).setValue( valueOf( new DataGenerator().getPhoneNumber() ) );
         $( "[class='checkbox__box']" ).click();
         $( "[type='button']" ).click();
         $( byText( "Забронировать" ) ).click();
